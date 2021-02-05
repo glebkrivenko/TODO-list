d527456a2c7244cc0db8f00f8c54991d7db64a43
Gleb <krivenko-gleb@mail.ru>
2021-02-05 22:13:24 +0300

    commit

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const progressBtn = document.querySelector("#progressBtn");
 const modalWrapper = document.querySelector(".modal__wrapper");
 const closeBtn = document.querySelector("#closeBtn");
 const okBtn = document.querySelector("#okBtn");
-let array = {
+const deleted = document.querySelector(".deleted");
+const inProgress = document.querySelector(".inProgress");
+const done = document.querySelector(".done");
+
+let data = {
   todo: [],
   progress: [],
   done: [],
@@ -17,7 +21,7 @@ let array = {
 
 const addingTask = () => {
   myTasks.innerHTML = "";
-  array.todo.forEach((item) => {
+  data.todo.forEach((item) => {
     myTasks.innerHTML += `<div class="todoNote">
 <p> Название задачи: <span class="note__name">${item.task__name}</span></p>
 <p> Описание: <span class="note__description">${item.task__description}</span></p>
@@ -28,17 +32,48 @@ const addingTask = () => {
   });
 };
 
+const deletedTasks = () => {
+  deleted.innerHTML = "";
+  data.del.forEach((item) => {
+    deleted.innerHTML += `<div class="delTodo">
+  <p> Название задачи: <span class="note__name">${item.task__name}</span></p>
+<p> Описание: <span class="note__description">${item.task__description}</span></p>
+</div>`;
+  });
+};
+
+const progressTasks = () => {
+  inProgress.innerHTML = "";
+  data.progress.forEach((item) => {
+    inProgress.innerHTML += `<div class="progressTodo">
+<p> Название задачи: <span class="note__name">${item.task__name}</span></p>
+<p> Описание: <span class="note__description">${item.task__description}</span></p>
+<button id="progressBtn"> <img src="image/done.png"></button>
+</div>`;
+  });
+};
+
+const doneTasks = () => {
+  done.innerHTML = "";
+  data.done.forEach((item) => {
+    done.innerHTML += `<div class="doneTodo">
+<p> Название задачи: <span class="note__name">${item.task__name}</span></p>
+<p> Описание: <span class="note__description">${item.task__description}</span></p>
+</div>`;
+  });
+};
+
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
   const taskName = document.querySelector("#task__name");
   const taskDescription = document.querySelector("#task__description");
-  array.todo.push({
+  data.todo.push({
     task__name: taskName.value,
     task__description: taskDescription.value,
   });
   addingTask();
   form.reset();
-  console.log(array);
+  console.log(data);
 });
 
 const editCard = (event) => {
@@ -47,12 +82,12 @@ const editCard = (event) => {
   const noteDesc = todoNotes.querySelector(".note__description").textContent;
   const modalTitle = document.querySelector("#edit-title");
   const modalDescription = document.querySelector("#edit-description");
-  const index = array.todo.findIndex(
+  const index = data.todo.findIndex(
     (item) =>
       item.task__name === noteName && item.task__description === noteDesc
   );
-  modalTitle.value = array.todo[index].noteName;
-  modalDescription.value = array.todo[index].noteDesc;
+  modalTitle.value = noteName;
+  modalDescription.value = noteDesc;
   openModal();
   okBtn.addEventListener("click", (event) => handleSubmitModal(event, index));
 };
@@ -61,12 +96,13 @@ const handleSubmitModal = (event, index) => {
   const modalNoteValue = document.querySelector("#edit-title").value;
   const modalNoteDescriptionValue = document.querySelector("#edit-description")
     .value;
-  array.todo.splice(index, 1, {
-    noteName: modalNoteValue,
-    noteDesc: modalNoteDescriptionValue,
+  data.todo.splice(index, 1, {
+    task__name: modalNoteValue,
+    task__description: modalNoteDescriptionValue,
   });
   closeModal();
-  //   addingTask();
+  addingTask();
+  console.log(data.todo);
 };
 const openModal = () => {
   modalWrapper.style.display = "block";
@@ -79,18 +115,54 @@ const deleteCard = (event) => {
   const todoNotes = event.target.closest(".todoNote");
   const noteName = todoNotes.querySelector(".note__name").textContent;
   const noteDesc = todoNotes.querySelector(".note__description").textContent;
-  const index = array.todo.findIndex(
+  const index = data.todo.findIndex(
     (item) =>
       item.task__name === noteName && item.task__description === noteDesc
   );
-  array.todo.splice(index, 1);
+  data.del.push(data.todo[index]);
+  data.todo.splice(index, 1);
+  console.log(data);
+  deletedTasks();
   addingTask();
 };
 
-list.addEventListener("click", (event) => {
+const progress = (event) => {
+  const todoNotes = event.target.closest(".todoNote");
+  const noteName = todoNotes.querySelector(".note__name").textContent;
+  const noteDesc = todoNotes.querySelector(".note__description").textContent;
+  const index = data.todo.findIndex(
+    (item) =>
+      item.task__name === noteName && item.task__description === noteDesc
+  );
+  data.progress.push(data.todo[index]);
+  data.todo.splice(index, 1);
+  console.log(data);
+  progressTasks();
+  addingTask();
+};
+
+const doneCard = (event) => {
+  const progressTodo = event.target.closest(".progressTodo");
+  const progressName = progressTodo.querySelector(".note__name").textContent;
+  const progressDesc = progressTodo.querySelector(".note__description")
+    .textContent;
+  const index = data.progress.findIndex(
+    (item) =>
+      item.task__name === progressName &&
+      item.task__description === progressDesc
+  );
+  data.done.push(data.progress[index]);
+  data.progress.splice(index, 1);
+  console.log(data);
+  doneTasks();
+  progressTasks();
+};
+
+myTasks.addEventListener("click", (event) => {
   if (event.target.closest("#deleteBtn")) deleteCard(event);
   if (event.target.closest("#editBtn")) editCard(event);
+  if (event.target.closest("#progressBtn")) progress(event);
 });
-closeBtn.addEventListener("click", () => {
-  closeModal();
+inProgress.addEventListener("click", (event) => {
+  if (event.target.closest("#progressBtn")) doneCard(event);
 });
